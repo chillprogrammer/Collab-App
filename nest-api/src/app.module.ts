228eb953b330f26bf4b0042f6832ffa52e874904
Domenico Maisano <dmaisano@snowjoe.com>
2019-11-09 21:56:27 -0500

    added in-memory-db

--- nest-api/src/app.module.ts
+++ nest-api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InMemoryDBModule } from '@nestjs-addons/in-memory-db';
 
 @Module({
-  imports: [],
+  imports: [InMemoryDBModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
