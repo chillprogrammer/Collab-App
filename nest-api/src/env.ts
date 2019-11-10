b34ec1e1013ecf222bf29b6f2ac124b3aacfee82
Domenico Maisano <dmaisano@snowjoe.com>
2019-11-09 21:49:22 -0500

    created nest app

---
+++ nest-api/src/env.ts
@@ -0,0 +1,4 @@
+export const env = {
+  BASE_URL: '10.202.133.180',
+  PORT: 3000,
+};
--- nest-api/src/main.ts
+++ nest-api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { env } from './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(env.PORT, env.BASE_URL);
+  console.log(`Nest API running on http://${env.BASE_URL}:${env.PORT}`);
 }
 bootstrap();
