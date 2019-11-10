b2ebc848ce039c10b1e3a0ca9a470b649c4be4ba
Domenico Maisano <dmaisano@snowjoe.com>
2019-11-09 20:50:49 -0500

    installed apollo server stuff

--- collab-api/src/app.module.ts
+++ collab-api/src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { NotesController } from './notes/notes.controller';
-// import { NotesService } from './notes/notes.service';
+import { GraphQLModule } from '@nestjs/graphql';
 import { NotesModule } from './notes/notes.module';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot({
+      // debug: false,
+      // playground: false,
+      autoSchemaFile: 'schema.gql',
+    }),
     MongooseModule.forRoot('mongodb://localhost:27017/collab-app', {
       useNewUrlParser: true,
     }),
